925f000f8a80fb9d181dc49e61eee890ad5fc359
Billy Riantono <billy@ruangguru.com>
2016-02-21 20:34:41 +0700

    fixing routes

--- src/routes.php
+++ src/routes.php
@@ -27,7 +27,7 @@ Route::group(['prefix' => config('youtube.route_base_uri')], function() {
 
 		$afterRedirectUri = config('youtube.after_redirect_uri');
 		if(isset($afterRedirectUri)){
-			return Redirect::to($afterRedirectUri,['code'=>$token]);
+			return redirect()->route($afterRedirectUri,['code'=>$token]);
 		} else {
 			return redirect('/');
 		}
